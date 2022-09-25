1450d5acd528409af735687e6fbadac1f03f0fd2
ngtinh <nguyenvanductinh77@gmail.com>
2022-09-25 18:41:04 +0700

    done UI travel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
+  darkMode: false,
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '35': "35px"
+      },
+      colors: {
+        gray33: "#333"
+      }
+    },
   },
   plugins: [],
 }
